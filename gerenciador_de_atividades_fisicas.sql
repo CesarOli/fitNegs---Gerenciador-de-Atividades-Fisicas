c2e38cc62f36e0d7fb04a0a7db28ca4d7ac266c6
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-15 19:58:16 -0300

    Adição da tabela 'atividadesFisicas' no banco de dados Gerenciador_de_Atividades_Fisicas.

--- gerenciador_de_atividades_fisicas.sql
+++ gerenciador_de_atividades_fisicas.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE Gerenciador_de_Atividades_Fisicas;
 
+USE Gerenciador_de_Atividades_Fisicas
+
+CREATE TABLE atividadesFisicas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    atividade_fisica VARCHAR(150),
+    tempo_da_atividade TIME,
+    distancia DECIMAL(8,2),
+    calorias_queimadas INT,
+    data_da_atividade DATE,
+    hora_da_atividade TIME
+);
+
