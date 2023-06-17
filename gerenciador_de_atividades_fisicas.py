cae4ce43861cda594a2e1a8e9ce88a55c1581d73
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-17 00:23:45 -0300

    criação da função que utiliza a tabela de atividades fisicas.

--- gerenciador_de_atividades_fisicas.py
+++ gerenciador_de_atividades_fisicas.py
@@ -19,12 +19,32 @@ else:
 
 def tabelaExiste(conexao, tabela):
     cursor = conexao.cursor()
-    cursor.execute(f'SHOW TABLES LIKES "{tabela}"')
+    cursor.execute(f'SHOW TABLES LIKE "{tabela}"')
     resultado = cursor.fetchone()
     cursor.close()
     return resultado is not None
+
+def usarTabelaAtividades(conexao):
+    cursor = conectar.cursor()
+    cursor.execute('CREATE DATABASE IF NOT EXISTS Gerenciador_de_Atividades_Fisicas')
+    cursor.execute('USE Gerenciador_de_Atividades_Fisicas')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS atividadesFisicas (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            atividade_fisica VARCHAR(150),
+            tempo_da_atividade TIME,
+            distancia DECIMAL(8,2),
+            calorias_queimadas INT,
+            data_da_atividade DATE,
+            hora_da_atividade TIME
+        )
+    ''')
+    cursor.close()
+
+usarTabelaAtividades(conectar)
+
     
-'''def dadosDasAtividades():
+def dadosDasAtividades():
     atividade = input('Informe a atividade praticada: ')
     tempo = input('Informe o tempo de duração desta atividade (HH:MM) : ')
     distancia = float(input('Informe a distancia percorrida nesta atividade: '))
@@ -32,6 +52,6 @@ def tabelaExiste(conexao, tabela):
     data = input('Informe a data da atividade: ')
     hora = input('Informe a hora em que você realizou esta atividade: ')
 
-    return atividade, tempo, distancia, calorias, data, hora'''
+    return atividade, tempo, distancia, calorias, data, hora
 
 conectar.close()
